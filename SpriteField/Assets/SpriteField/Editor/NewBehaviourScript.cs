5d3b46633e142a206e198386329e08807d87ad4e
Keigo Ando <anchan828@gmail.com>
2014-03-19 20:39:27 +0900

    Sample追加

---
+++ SpriteField/Assets/SpriteField/Editor/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class NewBehaviourScript : EditorWindow
+{
+    [MenuItem("Window/SpriteEditor")]
+    static void Open()
+    {
+        GetWindow<NewBehaviourScript>();
+    }
+
+    private Sprite sprite1,sprite2;
+    void OnGUI()
+    {
+        sprite1 = CustomEditorGUI.SpriteField(new Rect(134, 1, 128, 128), sprite1);
+        sprite2 = CustomEditorGUILayout.SpriteField(sprite2, GUILayout.Width(128), GUILayout.Height(128));
+    }
+}
