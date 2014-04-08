dc969ed5a4d4b552d5c3ca82683ce3b6001bf61c
Ryota Murohoshi <ryota.murohoshi@fuller.co.jp>
2014-04-08 23:37:31 +0900

    defined property instead of public field

--- PrefabExtension/Assets/PrefabExtension/Prefab.cs
+++ PrefabExtension/Assets/PrefabExtension/Prefab.cs
@@ -4,5 +4,8 @@ using System.Collections;
 [System.Serializable]
 public class Prefab
 {
-	public GameObject gameObject ;
+	public GameObject gameObject { get { return _gameObject; } }
+	// This field is assigned from Editor.
+	[SerializeField]
+	GameObject _gameObject;
 }
